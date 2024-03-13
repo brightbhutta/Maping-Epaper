33e1cc81344a11d9096a5a9a865fa4704e8644af
Tanveer Bhutta <39510752+brightbhutta@users.noreply.github.com>
2024-03-13 21:19:29 +0500

    Maping-Epaper

---
+++ epaper/js/exp.js
@@ -0,0 +1,18 @@
+$(function() {
+	$('#image-map').mapster({
+	    staticState: true,
+	    fillColor: "d42e16",
+	    stroke: true,
+	    strokeColor: "ff0000",
+	    strokeOpacity: 1.0,
+	    strokeWidth: 2,
+	    render_highlight:  {
+	        fillOpacity: 0.5,
+	        fillColor: "FFeedd",
+	        stroke: true,
+	        strokeOpacity: 1.0,
+	        strokeColor: "FF0000",
+	        strokeWidth: 2
+	    }
+	});
+});
